2ed5c7603619ee041c832a4d2065d3edc461f7b9
habib <ha9.0bib90@gmail.com>
2023-06-20 17:48:06 +0200

    rrty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  affAcount=false
+
   title = 'heartConsultant';
+  ngOnInit(): void {
+    
+  }
+  aff(){
+    this.affAcount=true
+  }
+  
 }
